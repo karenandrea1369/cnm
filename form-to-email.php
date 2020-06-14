36b42fce75eb901a1ba133b7c3f726458efa942d
karenandrea1369 <karenandrea12204@gmail.com>
2020-06-14 11:18:54 -0500

    FirstScreen con css

--- form-to-email.php
+++ form-to-email.php
@@ -1,19 +1,17 @@
 <?php
+
+if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
 
     $email_from = 'karenandrea12204@gmail.com';
-
     $email_subject = "Prueba";
-
     $email_body = "You have received a new message from the user $name.\n".
                             "Here is the message:\n $message".
 
     $to = "karenandrea12204@gmail.com";
-
     $headers = "From: $email_from \r\n";
-    
     $headers .= "Reply-To: $visitor_email \r\n";
 
     function IsInjected($str){
@@ -46,4 +44,5 @@
     }
     
     mail($to,$email_subject,$email_body,$headers);
+}
 ?>
